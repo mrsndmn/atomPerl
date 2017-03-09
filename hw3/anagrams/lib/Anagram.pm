80254e019af652c071387192ddd0f348a5f505bc
MrSandman <mrsndmn46@gmail.com>
2017-03-09 13:05:21 +0300

    test works

--- hw3/anagrams/lib/Anagram.pm
+++ hw3/anagrams/lib/Anagram.pm
@@ -4,6 +4,16 @@ use 5.010;
 use strict;
 use warnings;
 
+use DDP;
+#use utf8;
+use List::Util qw(all first none any);
+use Devel::Peek;
+use Encode qw(encode decode);
+
+BEGIN {
+    binmode(STDOUT,':utf8');
+}
+
 =encoding UTF8
 
 =head1 SYNOPSIS
@@ -39,15 +49,69 @@ anagram(['пятак', 'ЛиСток', 'пятка', 'стул', 'ПяТаК', '
 
 =cut
 
+$, = ', ';
+# my $k = "�";
+# $k = decode('utf8', $k);
+# say $k;
+# say "\x{fffd}";
+# Dump $k;
+#anagram (['ЛиСток', 'слиток']);
+
 sub anagram {
     my $words_list = shift;
     my %result;
+    my ($word, $anagram, $key);
 
-    #
-    # Поиск анограмм
-    #
+   @$words_list = map {decode ('utf-8', $_)} @$words_list;
+
+    for (my $i=0; $i < scalar (@$words_list); $i++){
+        
+        $word = $words_list->[$i];        
+        next if (any { isAnagram($word, decode ('UTF8', $_)) } keys %result);
+        
+        for (my $j=$i+1; $j < scalar(@$words_list); $j++){
+
+            $anagram = $words_list->[$j];
+            #say $word, $anagram;
+
+            if (isAnagram($word, $anagram)){
+
+                $key = first { isAnagram($anagram, decode ('UTF8', $_)) } keys %result;
+
+                if (defined $key && none { (lc $anagram) eq $_ } @{$result{$key}}) {
+                    push @{$result{$key}}, lc $anagram;
+               } elsif (!defined $key) {
+                    push @{$result{encode ('utf8',lc $word)}}, lc $word, lc $anagram;                    
+                }
+                
+            }
+        #
+        }   
+
+    }
+    
+
+    foreach $key (keys %result) {
+        if (scalar(@{$result{$key}} == 1)){
+            delete $result{$key};
+        } else{
+            @{$result{$key}} = map {encode ('utf8', $_)} sort @{$result{$key}};
+        }
+    }
+
 
     return \%result;
 }
+
+sub isAnagram {
+    my $word = shift;
+    my $anagram = shift;
+
+    return (
+        length ($word) == length ($anagram) &&
+        all {$word =~ /$_/i == $anagram =~ /$_/i} split //, $anagram
+        );
+
+} 
 
 1;
