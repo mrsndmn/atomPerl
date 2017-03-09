3e6538971e0868c39d1f39a5d3c94690341e2338
MrSandman <mrsndmn46@gmail.com>
2017-03-10 00:57:26 +0300

    anagram from Dictionary redone

--- hw3/anagrams/lib/Anagram.pm
+++ hw3/anagrams/lib/Anagram.pm
@@ -16,86 +16,80 @@ BEGIN {
 
 =encoding UTF8
 
-=head1 SYNOPSIS
+    =head1 SYNOPSIS
 
-Поиск анаграмм
+    Поиск анаграмм
 
-=head1 anagram($arrayref)
+    =head1 anagram($arrayref)
 
-Функцию поиска всех множеств анаграмм по словарю.
+    Функцию поиска всех множеств анаграмм по словарю.
 
-Входные данные для функции: ссылка на массив - каждый элемент которого - слово на русском языке в кодировке utf8
+    Входные данные для функции: ссылка на массив - каждый элемент которого - слово на русском языке в кодировке utf8
 
-Выходные данные: Ссылка на хеш множеств анаграмм.
+    Выходные данные: Ссылка на хеш множеств анаграмм.
 
-Ключ - первое встретившееся в словаре слово из множества
-Значение - ссылка на массив, каждый элемент которого слово из множества, в том порядке в котором оно встретилось в словаре в первый раз.
+    Ключ - первое встретившееся в словаре слово из множества
+    Значение - ссылка на массив, каждый элемент которого слово из множества, в том порядке в котором оно встретилось в словаре в первый раз.
 
-Множества из одного элемента не должны попасть в результат.
+    Множества из одного элемента не должны попасть в результат.
 
-Все слова должны быть приведены к нижнему регистру.
-В результирующем множестве каждое слово должно встречаться только один раз.
-Например
+    Все слова должны быть приведены к нижнему регистру.
+    В результирующем множестве каждое слово должно встречаться только один раз.
+    Например
 
-anagram(['пятак', 'ЛиСток', 'пятка', 'стул', 'ПяТаК', 'слиток', 'тяпка', 'столик', 'слиток'])
+    anagram(['пятак', 'ЛиСток', 'пятка', 'стул', 'ПяТаК', 'слиток', 'тяпка', 'столик', 'слиток'])
 
-должен вернуть ссылку на хеш
+    должен вернуть ссылку на хеш
 
 
-{
-    'пятак'  => ['пятак', 'пятка', 'тяпка'],
-    'листок' => ['листок', 'слиток', 'столик'],
-}
+    {
+        'пятак'  => ['пятак', 'пятка', 'тяпка'],
+        'листок' => ['листок', 'слиток', 'столик'],
+    }
 
 =cut
 
 $, = ', ';
 
+#anagram (['ЛиСток', 'слиток']);
+
 sub anagram {
     my $words_list = shift;
     my %result;
-    my ($word, $anagram, $key);
+    my %abcHash;
+    my ($abcword, $k, $key, $abcKey,$word);
 
    @$words_list = map {decode ('utf-8', $_)} @$words_list;
 
-    for (my $i=0; $i < scalar (@$words_list); $i++){
-        
-        $word = lc $words_list->[$i];        
-        next if (any { isAnagram($word,  $_) } keys %result);
-        
-        for (my $j=$i+1; $j < scalar(@$words_list); $j++){
+   # каждому слову сопоставляем слово из тех же букв но поставленных в алфвовитном порядке
+   # все добро в хэш, тогда в значение будем добавлять ключ 1 попавшегося слова и через него пушить в %result 
+   # хэш хэшэй хэшэй хэшэй
+    foreach my $i (0..$#$words_list) {
+        $word = lc $$words_list[$i];
+        $abcKey = toABC($word);
 
-            $anagram = lc $words_list->[$j];
-            next if $anagram eq $word;
-            #say $word, $anagram;
+        if (!exists $abcHash{$abcKey}) {
+            $abcHash{$abcKey}->{'keyWord'} = $word;
+        }
 
-            if (isAnagram($word, $anagram)){
-
-                $key = first { isAnagram($anagram, $_) } keys %result;
-                
-                if (defined $key && none {  $anagram eq $_ } @{$result{$key}}) {
-                    push @{$result{$key}}, $anagram;
-               } elsif (!defined $key) {
-                    push @{$result{$word}},$word, $anagram;                    
-                }
-                
-            }
-        #
-        }   
+        $abcHash{$abcKey}->{'hArr'}->{$word} = 1;        
 
     }
-    
 
-    foreach $key (keys %result) {
-        if (scalar(@{$result{$key}} == 1)){
-            delete $result{$key};
-        } else{
-            @{$result{$key}} = sort @{$result{$key}};
+    foreach $key (keys %abcHash) {
+        if (scalar(keys %{$abcHash{$key}->{'hArr'}}) > 1){
+            #p $abcHash{$key};
+            $k = $abcHash{$key}->{'keyWord'};
+            $result{$k} = [ sort keys %{$abcHash{$key}->{'hArr'}}];
         }
     }
 
-    #p %result;
+
     return \%result;
+}
+
+sub toABC {
+    return join ('', sort split (//, shift)); 
 }
 
 sub isAnagram {
@@ -104,8 +98,7 @@ sub isAnagram {
 
     return (
         length ($word) == length ($anagram) &&
-        # честно говоря, способ странный, но зато короткий
-        all { $word =~ s/$_// } split //, $anagram
+        all {$word =~ s/$_//i} split //, $anagram
         );
 
 } 
