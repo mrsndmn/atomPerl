1852427341ccd80796b6e3f847ef8c88b60bcaf6
MrSandman <mrsndmn46@gmail.com>
2017-03-21 12:34:11 +0300

    lec06

--- myconst/lib/myconst.pm
+++ myconst/lib/myconst.pm
@@ -3,6 +3,10 @@ use 5.022;
 use warnings;
 
 use DDP;
+
+use strict;
+use warnings;
+
 =encoding utf8
 
 =head1 NAME
@@ -14,92 +18,6 @@ use DDP;
     Version 1.00
 
 =cut
-our $VERSION = '1.00';
-    
-    $, = ', ';
-
-# блиииин, че-то 5 тест вообще все перевернул
-
- sub import {
-    my $self = shift;
-    my $caller = caller;
-    
-    my %wanted = @_;    
-    #p %wanted;
-
-    if (!scalar( keys %wanted ) ) {return;}
-    say '%wanted not empty';
-
-    while (scalar @_) {
-        my $key = shift;
-        my $val = shift;
-        
-        if ( ref $key ne '' || notValid($key) ) {
-            die "Bad arguments!\n";
-        }
-            
-        no strict 'refs';            
-        
-        if (ref $val eq 'HASH') {
-
-            if (notValid($val)) {
-                die "Bad arguments!\n";                
-            }
-
-            foreach my $subname (keys %$val) {
-                say "2) ".$val, $subname, ref $subname;
-                
-                if (ref $val->{$subname} ne '' ||
-                             notValid($subname))    {
-                    die "Bad argument $subname\n";                
-                }
-                
-                *{"$caller::$subname"} = sub() {$val->{$subname}}
-            }
-        }
-
-        elsif (ref $val eq '')  {
-            say "3) ", $val;           
-            *{"$caller::$key"} = sub() {$val}
-        } else {
-            die "Bad argument $key\n";
-        }
-    }
-    require strict;        
- }
-
-sub notValid {
-    my $str = shift;
-    return (!defined $str) || ($str =~ m/^$ | ^\d | [@\'\"\\\/] /x);
-}        
- 
-
-        # if ( any {ref $_ ne 'HASH' && ref $_ ne ''} keys %wanted) {
-        #         #say ref $name ne 'HASH' , ref $name ne '';
-        #         die "Bad argument $_\n";                
-        #     }
-
-        # no strict 'refs';            
-        # foreach my $name (keys %wanted){
-
-        #     if (ref $wanted{$name} eq 'HASH') {
-        #         foreach my $subname (keys %{$wanted{$name}}) {
-        #             say "2) ", $name, $subname;
-        #             *{"$caller::$subname"} = sub() {$wanted{$name}->{$subname}}
-        #         }
-        #     }
-
-        #     elsif (ref $wanted{$name} eq '')  {
-        #         say "3) ", $name;           
-        #         *{"$caller::$name"} = sub() {$wanted{$name}}
-        #     } else {
-        #         die "Bad argument $name\n";
-        #     }
-
-
-        # }
-        # require strict;
-
 =head1 SYNOPSIS
     package aaa;
 
@@ -118,6 +36,63 @@ sub notValid {
     print PI;               # 3.14
 =cut
 
+our $VERSION = '1.00';
+    
+    $, = ', ';
 
+# блиииин, че-то 5 тест вообще все перевернул
 
+ sub import {
+    my $self = shift;
+    my $caller = caller;
+    
+    my %wanted = @_ if scalar(@_)>0;    
+    #p %wanted;
+
+    if (!scalar( keys %wanted ) ) {return;}
+    #say '%wanted not empty';
+
+    while (scalar @_) {
+        my $key = shift;
+        my $val = shift;
+        
+        if ( ref $key ne '' || notValid($key) ) {
+            die "Bad arguments!\n";
+        }
+            
+        no strict 'refs';            
+        
+        if (ref $val eq 'HASH') {
+
+            if (notValid($val)) {
+                die "Bad arguments!\n";                
+            }
+
+            foreach my $subname (keys %$val) {
+                #say "2) ".$val, $subname, ref $subname;
+                
+                if (ref $val->{$subname} ne '' ||
+                             notValid($subname))    {
+                    die "Bad argument $subname\n";                
+                }
+                
+                *{"$caller::$subname"} = sub() {$val->{$subname}}
+            }
+        }
+
+        elsif (ref $val eq '')  {
+            #say "3) ", $val;           
+            *{"$caller::$key"} = sub() {$val}
+        } else {
+            die "Bad argument $key\n";
+        }
+    }
+    require strict;        
+ }
+
+sub notValid {
+    my $str = shift;
+    return (!defined $str) || ($str =~ m/^$ | ^\d | [@\'\"\\\/] /x);
+
+}
 1;
--- st.pl
+++ st.pl
@@ -1,22 +1,77 @@
-use feature 'say';
-use DDP;
-$str = '185.168.176.199 [03/Mar/2017:18:32:44 +0300] "GET /music/search/03:22%20%D0%9D%D0%A0%D0%90%D0%9 HTTP" 414 458 "-" "-" "-"';
-$str =~ m/^(?<ip>(?:\d{1,3}\.){3} \d{1,3}) \s* 
-                            \[ 
-                                (?<date>\d\d) \/ (?<month>\w\w\w) \/ (?<year>\d{4}) \: 
-                                (?<hour>\d\d) \: (?<minute>\d\d) \: (?<second>\d\d) \s
-                                (?<offset>[\-\+].{4})
-                            \] \s*
-                            
-                            \"(?:   (?<method>\w+) \s*
-                                    (?<smthElse>.+)
-                            )\" \s*
-                            
-                            
-                            (?<status>\d{3}) \s*
-                            (?<bytes>\d+) \s*
-                            \"(?<refferer>.*?)\" \s*
-                            \"(?<userAgent>.*?)\" \s*
-                            \"(?<ratio>.*?)\"$
-                            /x;
-p %+;
+use 5.022;
+use strict;
+use IO::Socket;
+my $server = IO::Socket::INET->new(
+    LocalPort => 8081,
+    Type => SOCK_STREAM,
+    ReuseAddr => 1,
+    Listen => 10)
+                    or die "Can't create server on port 8081 : $@ $/";
+#$|++;
+    $server->autoflush(1);
+
+while(my $client = $server->accept()){
+    $client->autoflush(1);
+    say "COnnected"; 
+    my $message = <$client>; 
+    chomp( $message );
+    print $client "Echo: ".$message;
+    close( $client );
+    last if $message eq 'END';
+}
+close( $server );
+
+
+
+
+
+
+
+
+__END__
+use strict;
+use IO::Socket;
+my $socket = IO::Socket::INET->new(
+    PeerAddr => 'search.cpan.org',
+    PeerPort => 80,
+    Proto => "tcp",
+    Type => SOCK_STREAM)
+                            or die "Can't connect to search.cpan.org $/";
+print $socket "GET / HTTP/1.0\nHost: search.cpan.org\n\n";
+while (<$socket>) {
+    print $_;
+}
+
+close
+
+
+
+__END__
+use strict;
+use POSIX qw(:sys_wait_h);
+$|=1;
+my ($r, $w);
+pipe($r, $w);
+$r->autoflush(1);
+$w->autoflush(1);
+
+if(my $pid = fork()){
+    close($r);
+    for (1..5) {
+        print $w $_ ;
+        warn "write ".$_;        
+    }
+    sleep (1);
+    close($w);
+    waitpid($pid, 0);
+}
+else {
+    die "Cannot fork $!" unless defined $pid;
+    close($w);
+    while(<$r>){
+        warn "read ".$_;
+        print $_."\n";
+    }
+    close($r);
+    exit;
+}
