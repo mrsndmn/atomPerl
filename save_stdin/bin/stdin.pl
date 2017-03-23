e00ec69b18ea197675ffaa392f897b6f838c1ae5
MrSandman <mrsndmn46@gmail.com>
2017-03-23 18:30:23 +0300

    smth wrong

--- save_stdin/bin/stdin.pl
+++ save_stdin/bin/stdin.pl
@@ -2,7 +2,63 @@
 
 use strict;
 use warnings;
-use Getopt::Long;
+use Getopt::Long qw(GetOptions);
+use Pod::Usage qw(pod2usage);
+use 5.022;
+use Data::Dumper;
 
-...
+my ($needHelp, $fileName);
 
+GetOptions (
+    "file=s" => \$fileName,
+    'help|?' => \$needHelp
+) or pod2usage(-exitval => 0, -verbose => 1);
+
+pod2usage(2) if ($needHelp || !defined $fileName);
+
+# die "No such file ${fileName}" if ( !(-e $fileName || -f $fileName || -r $fileName || !-z $fileName));
+
+
+$SIG{'INT'} = \&secondChance;
+
+open (my $fh, "+>", $fileName) or die "Cant get fileHandler";
+$fh->autoflush(1);
+
+die "Cannot interactive " if !is_interactive();
+
+say "Get ready";
+
+while(is_interactive()) {
+    my $echo = <>;
+    chop $echo;
+    say $echo;
+    $SIG{'INT'} = \&secondChance;
+    #Dump $SIG{'INT'};
+    print $fh, $echo;
+}
+
+$fh->close();
+
+
+sub secondChance {
+    print STDERR "Double Ctrl+C for exit";
+    $SIG{'INT'} = 'DEFAULT';   
+    return unless defined(<>)
+}
+
+sub is_interactive {
+return -t STDIN && -t STDOUT;
+}
+
+__END__
+=head1 NAME
+=for pod2usage:
+save_stdin - Script with save_stdin
+try '--help' to get more info
+
+=head1 SYNOPSIS
+stdin [options] [file ...]
+Options:
+--help                  : this help message
+--file [path_to_file]   : file for logging stdin
+=cut
