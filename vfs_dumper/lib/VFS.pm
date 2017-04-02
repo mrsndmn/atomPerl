017d98e0b0b81c50907676517d1a4eec5f385e57
MrSndmn <MrSndmn46@gmail.com>
2017-04-02 15:26:21 +0300

    not very short solution

--- vfs_dumper/lib/VFS.pm
+++ vfs_dumper/lib/VFS.pm
@@ -2,7 +2,7 @@ package VFS;
 use utf8;
 use strict;
 #use warnings;
-use 5.010;
+use 5.024;
 use File::Basename;
 use File::Spec::Functions qw{catdir};
 use JSON::XS;
@@ -12,9 +12,6 @@ use Devel::Peek;
 use DDP;
 use List::Util qw(any);
 #use Types::Serialier;
-
-# use JSON::XS::True;
-# use JSON::XS::False;
 
 no warnings 'experimental::smartmatch';
 
@@ -27,10 +24,11 @@ sub cut {			# i think, exist another way, but i dont know it ((
 sub mode2s {
 		my $rights = shift;
 
+		my $json = JSON::XS->new();
+
 		my %mode;
 		foreach my $who (qw(other group user)) {
 			foreach my $what (qw(execute write read)) {
-				my $json = JSON::XS->new();
 				$mode{$who}->{$what} = $rights % 2 ? $json->true : $json->false ;
 				$rights >>= 1;
 			}	
@@ -39,35 +37,20 @@ sub mode2s {
 		return \%mode;
 }
 
-sub getName {
-	my $bufref = shift;
-	
-	my $nameLenght = unpack "n", $$bufref;
-	cut ($bufref, 2);
-
-	my $name =  pack "U${nameLenght}",  unpack "W${nameLenght}", $$bufref;
-	cut ($bufref, $nameLenght);
-	
-	return decode("utf8", $name);
-}
-
 sub parse {
-	#$, = ", ";
 	my $buf = shift;
-	my $res = {};	
+	my $res = {};
 	#Dump $buf;
 	
-	my @history;	# to avoid recursion
-					# хотя с рекурсией решение, может быть, было бы более элегантным
-					# но я с расчетом на то, что может быть большая вложенность, забьется стэк -> stack overflow, deep recursion по перловому
-					# и да, с рекурсией все-таки можно, сделать, не знаю, почему я засомневался. Только в этом варианте надо продумать еще
-					# пару моментов. 
+	my @history;	
+	my %list;		# keys -> ref of dir
+					# value -> hash of names
+
 	while (length($buf)) {
-		my $op =chr(unpack "c", $buf);
+		my $op;
+		($op, $buf) = unpack "A(a*)", $buf;
+		
 		#Dump $buf;
-		cut (\$buf, 1); 	# мне это не нравится, наверняка 
-							# должен быть какой-то способ, про который я не нашел ничего, чтобы тоже самое делать без лишних телодвижений
-							# но как?
 		#warn $op;
 
 		switch ($op) {  
@@ -77,21 +60,17 @@ sub parse {
 				$dir->{'type'} = 'directory';
 				$dir->{'list'} = [];
 
-				my $name = getName(\$buf);
-
-				# для этого дела хорошо бы завести хэш, но мне кажется, в настоящей vfs
-				# list или как-то сразу сортируется, или 
-				# 'any' from List::Util
+				my $name;
+				($name, $buf) = unpack("n/A(a*)", $buf);
 				die "Such directory already exists" if (any { $_->{'type'} eq 'directory' and $_->{'name'} eq $name } @{$res->{'list'}});
-
-				$dir->{'name'} = $name;
+				$dir->{'name'} = decode("utf8", $name);
 				#say "DIR: ", $dir->{'name'};
 
-				my $rights = unpack "n", $buf;
-				cut (\$buf, 2);
+				my $rights;
+				($rights, $buf) = unpack "n(a*)", $buf;
 				$dir->{'mode'} = mode2s($rights);
 
-				if (exists $res->{'list'}){
+				if (exists $res->{'list'}) {
 					push @{$res->{'list'}}, $dir; 
 				} else {
 					# if its root directory
@@ -107,24 +86,22 @@ sub parse {
 				my $file; 
 				$file->{'type'} = 'file';
 
-				my $name = getName(\$buf);
-				
+				my $name;
+				($name, $buf) = unpack("n/A(a*)", $buf);
 				die "Such file already exists" if (any { $_->{'type'} eq 'file' and $_->{'name'} eq $name } @{$res->{'list'}});				
-				
-				$file->{'name'} =  $name;
+				$file->{'name'} =  decode("utf8", $name);
 				#say "FILE: ", $file->{'name'};
 				
-				my $rights = unpack "n", $buf;
-				cut (\$buf, 2);
+				my $rights;
+				($rights, $buf) = unpack "n(a*)", $buf;
 				$file->{'mode'} = mode2s($rights);
-				## the same with dir
 
-				my $size = unpack "N", $buf;
-				cut (\$buf, 4);
+				my $size;
+				($size, $buf) = unpack "N(a*)", $buf;
 				$file->{'size'} = $size;
 				
-				my $sha1 =  unpack "H40",  $buf;
-				cut (\$buf, 20);
+				my $sha1;
+				($sha1, $buf) =  unpack "H40(a*)",  $buf;
 				$file->{'hash'} = $sha1;
 
 				push @{$res->{'list'}}, $file;
@@ -152,10 +129,7 @@ sub parse {
 					$res->{'list'}->[$last] = $oldDir;
 					#p $res;
 				} else {
-					if ('Z' eq chr(unpack "c", $buf)) {
-						#warn "ok, you are in root";
-						# but it changes nothing, i think
-					} else {
+					if ('Z' ne unpack "A", $buf) {
 						die "cant go upper here. Alredy in root. ";
 					}
 				}
@@ -166,10 +140,8 @@ sub parse {
 				if (length($buf)) {
 					die "Garbage ae the end of the buffer";
 				} elsif (scalar(@history)>1) {
-					#p @history;
-					# or do it auto?
 					# 
-					die "you should up to root";
+					1 or die "you should up to root";
 				}
 				return $res;
 			}
@@ -178,10 +150,8 @@ sub parse {
 				die "invalid bin";
 			}
 		}
-	
 	}
 	die "binary must ended with Z"
-
 }
 
 1;
