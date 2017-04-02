dd3dd396fa49e8b0d09aed1370d5e87558db791f
MrSndmn <MrSndmn46@gmail.com>
2017-04-02 14:08:16 +0300

    little crow

--- crawler/lib/Crawler.pm
+++ crawler/lib/Crawler.pm
@@ -9,6 +9,7 @@ use Web::Query;
 use URI;
 
 use DDP;
+no warnings 'once';
 
 =encoding UTF8
 
@@ -22,19 +23,19 @@ Web Crawler
 
 =head1 run($start_page, $parallel_factor)
 
-Сбор с сайта всех ссылок на уникальные страницы
+    Сбор с сайта всех ссылок на уникальные страницы
 
-Входные данные:
+    Входные данные:
 
-$start_page - Ссылка с которой надо начать обход сайта
+    $start_page - Ссылка с которой надо начать обход сайта
 
-$parallel_factor - Значение фактора паралельности
+    $parallel_factor - Значение фактора паралельности
 
-Выходные данные:
+    Выходные данные:
 
-$total_size - суммарный размер собранных ссылок в байтах
+    $total_size - суммарный размер собранных ссылок в байтах
 
-@top10_list - top-10 страниц отсортированный по размеру.
+    @top10_list - top-10 страниц отсортированный по размеру.
 
 =cut
 
@@ -45,7 +46,7 @@ sub run {
     my ($start_page, $parallel_factor) = @_;
     # $start_page or die "You must setup url parameter";
     # $parallel_factor or die "You must setup parallel factor > 0";
-    $start_page = "http://www.ya.ru" if ! $start_page;
+    $start_page = "https://www.github.com" if ! $start_page;
     $parallel_factor = 4 if ! $parallel_factor;
 
     $AnyEvent::HTTP::MAX_PEER_HOST = $parallel_factor;
@@ -57,23 +58,20 @@ sub run {
     #$cv->begin;
 
 
-    my $guard; $guard = sub {
-        # my $cnt = 0;
-        # say $cnt++;
-        $cv->begin;
-        http_head ($start_page, 
-            recurse => 4,
-            on_header => sub {
-            p $_[0]->{'URL'};
-            $cv->end;            
-            }, 
-            sub {
-                print "in cb";
-                $cv->end;
-            });
+    $cv->begin;
+    http_head ($start_page, 
+        #recurse => 0,
+        on_header => sub {
+        p $_[0]->{'URL'};
+        $cv->end;            
+        }, 
+        sub {
+            p @_;
+            print "in cb\n";
+            $cv->end;
+    });
 
-    };
-    $guard->();
+    
     #$cv->end;
     $cv->recv;
 
