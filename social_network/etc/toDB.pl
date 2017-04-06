512572c21b948d43a6ab037e734914ffda6428f0
MrSandman <mrsndmn46@gmail.com>
2017-04-06 14:05:07 +0300

    dbase soon will be сomplete

--- social_network/etc/toDB.pl
+++ social_network/etc/toDB.pl
@@ -1,7 +1,25 @@
 use strict;
 use warnings;
 
-use 5.022;
+use 5.020;
 use DDP;
+use DBI;
+use FindBin;
+use IO::Uncompress::Unzip;
 
-$dbh = DBI->connect("dbi:SQLite:dbname=social.db", "","");
+my $dbFile = "$FindBin::Bin/social.db";
+my $dbh = DBI->connect("dbi:SQLite:dbname=$dbFile", "","", { RaiseError => 1 }) or die;
+
+#open my $fh, "<:zip", "$FindBin::Bin/user.zip";
+my $z = IO::Uncompress::Unzip->new( "$FindBin::Bin/user.zip" or die "unzip failed((\n") ;  
+
+my $sth = $dbh->prepare('INSERT INTO users (name) VALUES( ? )');
+my $c = 0;
+while(my $line = $z->getline and $c < 5) {
+    $c++;
+    chomp $line;
+    $sth->execute($line);
+} 
+
+
+
