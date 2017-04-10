7f75447294d3765a0fd355da4bcbe4853f0ee47d
MrSandman <mrsndmn46@gmail.com>
2017-04-10 03:09:55 +0300

    no switch viva given

--- proxy/bin/proxy.pl
+++ proxy/bin/proxy.pl
@@ -34,60 +34,48 @@ $g = tcp_server undef, 8081,
                     # no regexp no overhead
                     my ($op, $other ) = split " ", $_[1], 2;
                     return if !defined $op;
-                    # no Switch no problems :: but i dont like this
-                    if ($op eq '?') {
-                        
-                        $h->push_write(join "", <DATA>);
-
-                    } elsif($op eq 'URL') {
-
-                        if (!defined($other)) {
-                            $h->push_write("Need argument.\nTry '?' to get help\n");
-                            return;
-                        } 
-                        if ( is_web_uri $other ) {
-                            $URL = $other;
-                            $h->push_write("OK\n");
-                            warn $URL;
-                        } else {
-                            $h->push_write("Invalid URL: $other\nURL must be absolute and begins with \'hhtp(s)://\'\nTry '?' to get help\n");
-                            return;
-                        }
-
-                    } elsif($op eq 'HEAD') {
-
-                        if (!defined($URL)) {
-                            $h->push_write("You should set URL\nTry '?' to get help\n");
-                            return;
-                        }
-                        head_request ($URL, sub {
-                                                my $ans = shift;
-                                                $h->push_write( $ans."\nOK\n" );
-                                                say "head OK ", length $ans;
-                                    });
-
-                    } elsif($op eq 'GET') {
-                        
-                        if (!defined($URL)) {
-                            $h->push_write("You should set URL\nTry '?' to get help\n");
-                            return;
-                        }
-                        get_request ($URL, sub {
-                                            my $ans = shift;
-                                            $h->push_write( $ans."\nOK\n" );
-                                            say "get OK ", length $ans;                                            
-                                    });
-
-                    } elsif($op eq 'FIN') {
-                        
-                        $h->push_write("Goodbye\n");
-                        $h->destroy;
+                    # no Switch no problems :: we have given when
                     
-                    } else {
+                    given ($op) {
+                        when('?') {
+                            $h->push_write(join "", <DATA>);                            
+                        }
+                        when ('URL') {
+                            if (!defined($URL)) {
+                                $h->push_write("You should set URL\nTry '?' to get help\n");
+                                return;
+                            }
+                            head_request ($URL, sub {
+                                                    my $ans = shift;
+                                                    $h->push_write( $ans."\nOK\n" );
+                                                    say "head OK ", length $ans;
+                                        });
+                        }
+                        when ('HEAD') {
+                            if (!defined($other)) {
+                                $h->push_write("Need argument.\nTry '?' to get help\n");
+                                return;
+                            } 
+                            if ( is_web_uri $other ) {
+                                $URL = $other;
+                                $h->push_write("OK\n");
+                                warn $URL;
+                            } else {
+                                $h->push_write("Invalid URL: $other\nURL must be absolute and begins with \'hhtp(s)://\'\nTry '?' to get help\n");
+                                return;
+                            }
+                        }
                         
-                        $h->push_write("Unknown operation: '${op}'\n");
+                        when 'FIN' {
+                            $h->push_write("Goodbye\n");
+                            $h->destroy;
+                        }
+
+                        default {
+                            $h->push_write("Unknown operation: '${op}'\n");
+                        }
                     
-                    }
+                    
 
                 })
             }
