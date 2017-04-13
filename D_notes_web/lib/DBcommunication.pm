6b26e61fa414bec6905410b8cfce8ef1d37f25ec
MrSndmn <MrSndmn46@gmail.com>
2017-04-14 02:11:44 +0300

    db, registration, half auth

---
+++ D_notes_web/lib/DBcommunication.pm
@@ -0,0 +1,58 @@
+package DBcommunication;
+
+use strict;
+use warnings;
+
+use DDP;
+use DBI;
+use FindBin;
+
+# DBcommunication->new( dbFile => 'dbFile' );
+sub new {
+    my ($class, %params) = @_;
+    die "You must define \'dbFile\' in constructor" if !exists $params{'dbFile'};
+    my $dbFile = "$FindBin::Bin/../data/".$params{'dbFile'};
+    die "No such file $dbFile" if ! -e $dbFile;
+    $params{'dbFile'} = $dbFile;
+
+    my $dbh = DBI->connect("dbi:SQLite:dbname=$dbFile", "","", { RaiseError => 1 }) or die "Cannot connect this db:\n";
+    warn "dbh created";
+    $params{'dbh'} = $dbh;
+    return bless \%params, $class;
+}
+
+sub isValid {
+    my ($self, $username, $password) = @_;
+    warn "VALIDATION", $username, " | ", $password;
+    my $dbh = $self->{'dbh'};
+    return $dbh->selectrow_arrayref(
+                    'SELECT COUNT(*) FROM auth WHERE username = (?) and password == (?)',
+                    {},
+                    $username,
+                    $password
+                    )->[0];
+}
+
+sub newUser {
+    my ($self, $username, $password) = @_;
+    warn "NEW USER", $username, " | ", $password;
+    my $dbh = $self->{'dbh'};
+    return 0 if $self->nick_exists($username);
+    my $sql = "INSERT INTO auth (username, password) VALUES( (?), (?) );";
+    my $sth = $dbh->prepare($sql);
+    $sth->execute($username, $password);
+    return 1;
+}
+
+sub nick_exists {
+    my ($self, $username) = @_;
+    warn "nick UNique check", $username;
+    my $dbh = $self->{'dbh'};
+    return $dbh->selectrow_arrayref(
+                    'SELECT COUNT(*) FROM auth WHERE username = (?)',
+                    {},
+                    $username,
+                    )->[0];
+}
+
+1;
--- D_notes_web/lib/notes_web.pm
+++ D_notes_web/lib/notes_web.pm
@@ -1,10 +1,71 @@
 package notes_web;
 use Dancer2;
+use Digest::MD5 qw(md5_hex);
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+
+use DBcommunication;
 
 our $VERSION = '0.1';
+
+my $db = DBcommunication->new(dbFile => 'web_note.db');
 
 get '/' => sub {
     template 'index' => { 'title' => 'Atom notes' };
 };
+
+post '/' => sub {
+    my $username = body_parameters->get('username');
+    my $password = md5_hex( body_parameters->get('password') );
+    #TODO valid & escape
+    if (length($username) < 4 or length($password) < 4) {
+        warn "SHORT PASSWD OR UNAME";
+        template 'index', {
+            'title' => 'Atom notes',
+            'err' => 'This username or rassword is too short',
+        };
+    } elsif (! $db->isValid($username, $password)) {
+        warn "wrong uname or passwd";
+        template 'register', {
+            'title' => 'Atom notes',
+            'err' => 'Wrong username or password',
+        };
+    } else {
+        session 'logged_in' => true;
+        template 'index', {
+            'title' => 'Atom notes',
+            'msg' => "Hello, ".$username,
+        }
+    };
+};
+
+get '/register' => sub {
+    template 'register' => { 'title' => 'Notes registration' };
+};
+
+post '/register' => sub {
+    my $username = body_parameters->get('username');
+    my $password = md5_hex( body_parameters->get('password') );
+    #TODO valid & escape
+    if (length($username) < 4 or length($password) < 4 or length($username) > 20 or length($password) > 20 ) {
+        warn "SHORT PASSWD OR UNAME";
+        template 'register', {
+            'title' => 'Notes registration',
+            'err' => 'This username or rassword is too short or too long',
+        };
+    } elsif ($db->newUser($username, $password)) {
+        redirect '/';
+        template 'index', {
+            'title' => 'Atom notes',
+            'msg' => 'Registration succeed',
+        };
+    } else {
+        template 'register', {
+            'title' => 'Atom notes',
+            'err' => 'This username is already in use',
+        };
+    }
+};
+
 
 true;
