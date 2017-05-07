388e1cc91d929d234973cf10782593d7498db60d
MrSandman <mrsndmn46@gmail.com>
2017-05-07 12:40:16 +0300

    tests begins

--- dbi-active-record/t/lib/Local/MusicLib/AlbumTest.pm
+++ dbi-active-record/t/lib/Local/MusicLib/AlbumTest.pm
@@ -2,50 +2,88 @@ package Local::MusicLib::AlbumTest;
 
 use Test::Class::Moose extends => 'Local::Test';
 
-use Test::Class::Moose;
+use Local::MusicLib::Album;
 
+my $dt = DateTime->now;
+
+my $artist;
+
+my $album;
+
+sub test_attributes {
+    my ($self) = @_;
+    $self->test_report->plan(1);
+
+    $artist = $self->{'artist'};
+
+    is($album->name, "The Menagerie Inside");
+    is($album->artist_id, $artist->id);
+    is($album->year, 1999);
+    is($album->type, "single");
+    is($album->create_time, $dt);
+}
 
 sub test_album_insert {
     my ($self) = @_;
-    use DDP;
-    warn p $self;    
-    pass "I tested something!";
+    $album = $self->{'album'}; 
+    my $insert_ok = $album->insert();
+    
+    ok($insert_ok);
+    
+    # my $serialize_dt = $dbh->selectrow_arrayref("SELECT create_time FROM album WHERE id = $id")->[0];
+    # is($serialize_dt, $dt->epoch, "artist's create_time serialized");
+
     return;
 }
 
 sub test_album_select {
     my ($self) = @_;
+    my $dbh = $self->{'dbh'};
 
+
+    return 1;
 }
 
 sub test_album_update {
     my ($self) = @_;
+    my $dbh = $self->{'dbh'};
 
+    return 1; 
 }
 
 sub test_album_delete {
     my ($self) = @_;
+    my $dbh = $self->{'dbh'};
 
+    return 1; 
 }
 
 sub test_album_select_by_artist_id {
     my ($self) = @_;
 
+    my $dbh = $self->{'dbh'};
+    return 1;
 }
 
 sub test_album_select_by_id {
     my ($self) = @_;
 
+    my $dbh = $self->{'dbh'};
+    return 1; 
 }
 
 sub test_album_select_by_name {
     my ($self) = @_;
 
+    my $dbh = $self->{'dbh'};
+    return 1;
 }
 
 sub test_album_select_by_type {
     my ($self) = @_;
 
+    my $dbh = $self->{'dbh'};
+    return 1;
 }
 
 1;
--- dbi-active-record/t/lib/Local/MusicLib/TrackTest.pm
+++ dbi-active-record/t/lib/Local/MusicLib/TrackTest.pm
@@ -2,42 +2,57 @@ package Local::MusicLib::TrackTest;
 
 use Test::Class::Moose extends => 'Local::Test';
 
-use Test::Class::Moose;
 
+use Local::MusicLib::Track;
+my $track = Local::MusicLib::Track->new();
 
 sub test_track_insert {
     my ($self) = @_;
-
+    my $dbh = $self->{'dbh'};
+    
+    return;
 }
 
 sub test_track_select {
     my ($self) = @_;
-
+    my $dbh = $self->{'dbh'};
+    
+    return;
 }
 
 sub test_track_update {
     my ($self) = @_;
-
+    my $dbh = $self->{'dbh'};
+    
+    return;
 }
 
 sub test_track_delete {
     my ($self) = @_;
-
+    my $dbh = $self->{'dbh'};
+    
+    return;
 }
 
 sub test_track_select_by_id {
     my ($self) = @_;
-
+    my $dbh = $self->{'dbh'};
+    
+    return;
 }
 
 sub test_track_select_by_album_id {
     my ($self) = @_;
-
+    my $dbh = $self->{'dbh'};
+    
+    return;
 }
 
 sub test_track_select_by_name {
     my ($self) = @_;
-
+    my $dbh = $self->{'dbh'};
+    
+    return;
 }
 
 1;
