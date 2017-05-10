9948277f01e3b312f7eb0c99edb7308afc384bbd
MrSndmn <MrSndmn46@gmail.com>
2017-05-10 18:34:33 +0300

    config support

--- dbi-active-record/lib/Local/MusicLib/DB/PostgreSQL.pm
+++ dbi-active-record/lib/Local/MusicLib/DB/PostgreSQL.pm
@@ -2,14 +2,32 @@ package Local::MusicLib::DB::PostgreSQL;
 use Mouse;
 extends 'DBI::ActiveRecord::DB::PostgreSQL';
 
-my $dbname = 'muslib';
-my $username = 'mrsndmn';
-my $password = '123qwe';
+# use Carp qw/confess/;
+use YAML::Tiny;
 
 sub _build_connection_params {
-    my ($self) = @_;
+    my ($self) = shift;
+
+    # its better to create new module conf reader, but copypaste easier
+    #TODO create conf reader module
+    my ($volume, $directory, $file) = File::Spec->splitpath(__FILE__);
+    my $conf = YAML::Tiny->read( "$directory../etc/config.yml" )->[0];
+    
+    # enter your db conf
+    my $DBMS = $conf->{DBMS};
+    die "please, change DBMS to 'postgres' in you conf if you really want to use it\n"
+        ."Or use Local::MusicLib::DB::SQLite in your entities" 
+        if $DBMS ne 'postgres';
+        
+    my $dbname = $conf->{dbname};
+    my $host = $conf->{host};
+    my $username = $conf->{username};
+    my $password = $conf->{password};
+
+    warn "define db configuration in config\nit's here: $directory../etc/config.yml\n" unless defined $dbname && $username && $password;
+
     return [
-        'dbi:Pg:dbname=$dbname;',
+        "dbi:Pg:dbname=$dbname;",
         $username,
         $password,
         { RaiseError => 1 },
--- dbi-active-record/lib/Local/MusicLib/DB/SQLite.pm
+++ dbi-active-record/lib/Local/MusicLib/DB/SQLite.pm
@@ -1,12 +1,24 @@
 package Local::MusicLib::DB::SQLite;
 use Mouse;
 extends 'DBI::ActiveRecord::DB::SQLite';
+use YAML::Tiny;
 
 sub _build_connection_params {
     my ($self) = @_;
-    # Foreign key constraints are disabled by default in sqlite
+
+    # its better to create new module conf reader, but copypaste easier
+    #TODO create conf reader module
+    my ($volume, $directory, $file) = File::Spec->splitpath(__FILE__);
+    my $conf = YAML::Tiny->read( "$directory../etc/config.yml" )->[0];
+    
+    # enter your db conf
+    my $DBMS = $conf->{DBMS};
+    die "please, change DBMS to 'sqlite' in you conf if you really want to use it" if $DBMS ne 'sqlite';
+    my $dbname = $conf->{dbname};
+
     return [
-        'dbi:SQLite:dbname=data/muslib.db', '', '', { RaiseError => 1 },
+        "dbi:SQLite:dbname=$dbname", '', '', { RaiseError => 1 },
+        # Foreign key constraints are disabled by default in sqlite
         "PRAGMA foreign_keys = ON;"
     ];
 }
